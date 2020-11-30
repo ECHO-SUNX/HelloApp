9acf89c752a761158d5713f9c57e5f247105f84a
sunx <583320490@qq.com>
2020-11-30 15:27:31 +0800

    land layout and save state

--- app/src/main/java/com/example/helloapp/MainActivity.java
+++ app/src/main/java/com/example/helloapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.helloapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,15 +14,25 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button buttonL,buttonR;
 
+    private static String LOG_TAG="mylog";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG,"onCreate...");
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.textView);
         buttonL = findViewById(R.id.button);
         buttonR = findViewById(R.id.button2);
 
-        textView.setText("hello world");
+        if (savedInstanceState!=null){
+            String butStr = savedInstanceState.getString("butStr");
+            if (butStr!=null){
+                textView.setText(butStr);
+            }
+        }else{
+            textView.setText("hello world");
+        }
 
         buttonL.setOnClickListener(v->{
             textView.setText("←");
@@ -31,5 +43,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("butStr",textView.getText().toString());
 
+    }
 }
